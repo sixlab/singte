3646c9889fc7b043463e8ebb4ca354a054ee23fb
nianqinianyi <nianqinianyi@163.com>
2022-01-12 15:24:20 +0800

    更改前端模板，这个模板是在模板之家下载的

---
+++ singte-core/src/main/resources/static/cssmoban/js/picview.js
@@ -0,0 +1,1 @@
+$(function () {    var swiper = new Swiper('.swiper-container', {        zoom: true,        pagination: {            el: '.swiper-pagination',        },        navigation: {            nextEl: '.swiper-button-next',            prevEl: '.swiper-button-prev',        },    });});
