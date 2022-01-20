dc6043566fe147c5cafd482182843151e968acdd
nianqinianyi <nianqinianyi@163.com>
2022-01-21 00:08:56 +0800

    supervisor-run.sh

---
+++ docs/supervisor-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../
+git pull
+mvn clean install -Dmaven.test.skip=true
+
+cd ../
+rm singte-web.jar
+mv ./singte/singte-web/target/singte-web.jar ./
+
+cmd=$(ps aux | grep "singte-web" | grep java | awk '{print $2}')
+echo -e "PID: \n\033[31m\033[05m$cmd\033[0m"
+echo "PID:$cmd"
+for id in ${cmd}; do
+  kill -9 ${id}
+  echo "kill $id"
+done
+echo 'finish kill'
