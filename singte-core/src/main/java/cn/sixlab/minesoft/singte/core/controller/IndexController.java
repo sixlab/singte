49d2559738ee1d5216f1375c4ea7f71eab0a1c1d
nianqinianyi <nianqinianyi@163.com>
2022-05-19 18:33:23 +0800

    用户登录

--- singte-core/src/main/java/cn/sixlab/minesoft/singte/core/controller/IndexController.java
+++ singte-core/src/main/java/cn/sixlab/minesoft/singte/core/controller/IndexController.java
@@ -6,14 +6,20 @@ import cn.hutool.captcha.generator.RandomGenerator;
 import cn.sixlab.minesoft.singte.core.common.config.BaseController;
 import cn.sixlab.minesoft.singte.core.common.utils.I18nUtils;
 import cn.sixlab.minesoft.singte.core.common.utils.StCacheHolder;
+import cn.sixlab.minesoft.singte.core.common.utils.StErr;
+import cn.sixlab.minesoft.singte.core.common.utils.TokenUtils;
 import cn.sixlab.minesoft.singte.core.common.vo.ModelResp;
 import cn.sixlab.minesoft.singte.core.dao.StPageDao;
 import cn.sixlab.minesoft.singte.core.models.StPage;
+import cn.sixlab.minesoft.singte.core.service.StUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,6 +32,9 @@ public class IndexController extends BaseController {
 
     @Autowired
     private StPageDao pageDao;
+
+    @Autowired
+    private StUserDetailsService userDetailsService;
 
     @GetMapping(value = "/about")
     public String article(ModelMap modelMap) {
@@ -58,14 +67,14 @@ public class IndexController extends BaseController {
 
     @ResponseBody
     @RequestMapping(value = "/captchaVerify")
-    public ModelResp captchaVerify(String key, String code)  {
+    public ModelResp captchaVerify(String key, String code) {
         key = "captcha_" + key;
         Object cached = StCacheHolder.CACHE_5m.get(key);
 
-        if(code.equals(cached)){
+        if (code.equals(cached)) {
             StCacheHolder.CACHE_5m.remove(key);
             return ModelResp.success();
-        }else{
+        } else {
             return ModelResp.error(0);
         }
     }
@@ -75,5 +84,27 @@ public class IndexController extends BaseController {
     public String initTest() {
 
         return I18nUtils.get("login.forbidden");
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/user/login")
+    public ModelResp login(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (null == userDetails) {
+            return ModelResp.error(StErr.NOT_EXIST, "login.user.none");
+        }
+
+        if (!BCrypt.checkpw(password, userDetails.getPassword())) {
+            return ModelResp.error(StErr.LOGIN_PWD_ERR, "login.err.pwd");
+        }
+
+        if (!userDetails.isEnabled()) {
+            return ModelResp.error(StErr.LOGIN_DISABLE, "login.err.disable");
+        }
+
+        final String token = TokenUtils.generateToken(userDetails);
+        userDetailsService.updateToken(username, token);
+
+        return ModelResp.success().add("Authorization", token);
     }
 }
