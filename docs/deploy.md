8d58fb9b27a8c20e6d89a642ea365e5deec82645
nianqinianyi <nianqinianyi@163.com>
2022-05-23 17:56:48 +0800

    重置默认密码

---
+++ docs/deploy.md
@@ -0,0 +1,3 @@
+--initPWD=q12345
+
+server_sh_path
--- singte-core/src/main/java/cn/sixlab/minesoft/singte/core/common/init/InitAdmin.java
+++ singte-core/src/main/java/cn/sixlab/minesoft/singte/core/common/init/InitAdmin.java
@@ -1,5 +1,6 @@
 package cn.sixlab.minesoft.singte.core.common.init;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.digest.BCrypt;
 import cn.sixlab.minesoft.singte.core.common.utils.StConst;
 import cn.sixlab.minesoft.singte.core.dao.StConfigDao;
@@ -7,6 +8,7 @@ import cn.sixlab.minesoft.singte.core.dao.StUserDao;
 import cn.sixlab.minesoft.singte.core.models.StConfig;
 import cn.sixlab.minesoft.singte.core.models.StUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,10 +22,14 @@ public class InitAdmin implements BaseInitComponent {
     @Autowired
     private StConfigDao configDao;
 
+    @Value("${initPWD}")
+    private String initPWD;
+
     @Override
     public void init() {
         StConfig config = configDao.selectByKey(StConst.ST_INIT);
         if (config == null) {
+            // 系统初始化，创建管理用户
             config = new StConfig();
             config.setConfigKey(StConst.ST_INIT);
             config.setConfigVal("1");
@@ -33,15 +39,31 @@ public class InitAdmin implements BaseInitComponent {
 
             configDao.save(config);
 
-            StUser stUser = new StUser();
+            initAdmin();
+        }else{
+            if (StrUtil.isNotEmpty(initPWD)) {
+                initAdmin();
+            }
+        }
+    }
+
+    private void initAdmin(){
+        StUser stUser = userDao.selectByUsername("singte");
+
+        if (stUser == null) {
+            stUser = new StUser();
             stUser.setUsername("singte");
             stUser.setShowName("singte");
-            stUser.setPassword(BCrypt.hashpw("123456"));
             stUser.setStatus(StConst.YES);
             stUser.setRole(StConst.ROLE_ADMIN);
             stUser.setCreateTime(new Date());
-
-            userDao.save(stUser);
         }
+
+        if (StrUtil.isEmpty(initPWD)) {
+            initPWD = "123456";
+        }
+        stUser.setPassword(BCrypt.hashpw(initPWD));
+
+        userDao.save(stUser);
     }
 }
